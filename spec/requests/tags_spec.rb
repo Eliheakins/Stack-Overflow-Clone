7dcb94bcd266ba74f3822c010e7996fcaa3e9e17
JacobKrehbiel03 <143643828+JacobKrehbiel03@users.noreply.github.com>
2024-10-28 00:08:57 +0000

    Trying tests for tags, however rspec is having difficulties to run

--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Tags", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /tags" do
+    it "works! (now write some real specs)" do
+      get tags_path
+      expect(response).to have_http_status(200)
+    end
   end
 end
