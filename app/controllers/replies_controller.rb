24a1c6bcd6b4b2dab8dbc0b620ace49fadf52424
atauber27 <atauber27@gmail.com>
2024-11-11 10:40:44 -0500

    Current user associated with posts/replies

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,5 +1,6 @@
 class RepliesController < ApplicationController
   before_action :set_reply, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: %i[ new create edit update destroy ]
 
   # GET /replies or /replies.json
   def index
