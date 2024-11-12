61877c691e9433962c6b188526b41499a40d31b3
atauber27 <atauber27@gmail.com>
2024-11-12 00:26:24 -0500

    Hidden destroy/edit buttons for unauthorized users

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,6 +1,7 @@
 class RepliesController < ApplicationController
   before_action :set_reply, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!, only: %i[ new create edit update destroy ]
+  before_action :authenticate_user!, only: %i[ new create ]
+  before_action :check_user_match, only: %i[ edit update destroy ]
 
   # GET /replies or /replies.json
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          authentication_keys: [:username]
-  enum :role, %i[student instructor]
+  enum :role, %i[student instructor admin]
   has_many :posts, dependent: :destroy
   has_many :replies, dependent: :destroy
 end
