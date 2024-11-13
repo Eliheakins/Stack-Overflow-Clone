81d11c19b1b89cd933c1f1ef69798a04fce7cf78
atauber27 <atauber27@gmail.com>
2024-11-13 16:05:44 -0500

    more user purging

--- db/migrate/20241023135638_create_posts.rb
+++ db/migrate/20241023135638_create_posts.rb
@@ -3,7 +3,6 @@ class CreatePosts < ActiveRecord::Migration[7.2]
     create_table :posts do |t|
       t.string :title
       t.string :text
-      t.references :user, null: false, foreign_key: true
       t.integer :votes
 
       t.timestamps
