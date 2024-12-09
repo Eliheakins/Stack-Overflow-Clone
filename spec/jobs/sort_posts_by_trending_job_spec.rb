e8bc64c929e7f5a4ae540e7ba444bdfb60ad218d
atauber27 <atauber27@gmail.com>
2024-12-09 11:55:46 -0500

    prior to whenever gem

--- spec/jobs/sort_posts_by_trending_job_spec.rb
+++ spec/jobs/sort_posts_by_trending_job_spec.rb
@@ -13,10 +13,8 @@ RSpec.describe SortPostsByTrendingJob, type: :job do
   end
 
   it "prioritizes posts from the past day over posts from previous days" do
-    post1 = double("Post")
-    post2 = double("Post")
     canned_time = Time.new("2024-09-31 01:00:00")
-    expect(post1).to receive(:created_at).and_return(canned_time)
-    expect(post2).to receive(:created_at).and_return(canned_time.yesterday)
+    post1 = FactoryBot.build(:post, :created_at => canned_time)
+    post2 = FactoryBot.build(:post, :created_at => canned_time.yesterday)
   end
 end
